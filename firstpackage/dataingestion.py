f0684563634a506ccc20c6b648c05f1f2a739636
petermartian <petmartian@gmail.com>
2024-11-03 21:55:42 +0100

    Update dataingestion.py

--- firstpackage/dataingestion.py
+++ firstpackage/dataingestion.py
@@ -1,3 +1,25 @@
+
+from sqlalchemy import create_engine, text
+import logging
+import pandas as pd
+
+# Name our logger so we know that logs from this module come from the data_ingestion module
+logger = logging.getLogger('data_ingestion')
+# Set a basic logging message up that prints out a timestamp, the name of our logger, and the message
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+db_path = 'sqlite:///Maji_Ndogo_farm_survey_small.db'
+
+sql_query = """
+SELECT *
+FROM geographic_features
+LEFT JOIN weather_features USING (Field_ID)
+LEFT JOIN soil_and_crop_features USING (Field_ID)
+LEFT JOIN farm_management_features USING (Field_ID)
+"""
+
+weather_data_URL = "https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Maji_Ndogo/Weather_station_data.csv"
+weather_mapping_data_URL = "https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Maji_Ndogo/Weather_data_field_mapping.csv"
+
 def create_db_engine(db_path):
     try:
         engine = create_engine(db_path)
