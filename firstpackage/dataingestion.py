560c0df2d402aaf2816ac5e9cfb93242ff2cf908
petermartian <petmartian@gmail.com>
2024-11-03 21:53:37 +0100

    Update dataingestion.py

--- firstpackage/dataingestion.py
+++ firstpackage/dataingestion.py
@@ -1,35 +1,45 @@
-from sqlalchemy import create_engine, text
-import logging
-import pandas as pd
-# Name our logger so we know that logs from this module come from the data_ingestion module
-logger = logging.getLogger('data_ingestion')
-# Set a basic logging message up that prints out a timestamp, the name of our logger, and the message
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-db_path = 'sqlite:///Maji_Ndogo_farm_survey_small.db'
+def create_db_engine(db_path):
+    try:
+        engine = create_engine(db_path)
+        # Test connection
+        with engine.connect() as conn:
+            pass
+        # test if the database engine was created successfully
+        logger.info("Database engine created successfully.")
+        return engine # Return the engine object if it all works well
+    except ImportError: #If we get an ImportError, inform the user SQLAlchemy is not installed
+        logger.error("SQLAlchemy is required to use this function. Please install it first.")
+        raise e
+    except Exception as e:# If we fail to create an engine inform the user
+        logger.error(f"Failed to create database engine. Error: {e}")
+        raise e
 
-sql_query = """
-SELECT *
-FROM geographic_features
-LEFT JOIN weather_features USING (Field_ID)
-LEFT JOIN soil_and_crop_features USING (Field_ID)
-LEFT JOIN farm_management_features USING (Field_ID)
-"""
+def query_data(engine, sql_query):
+    try:
+        with engine.connect() as connection:
+            df = pd.read_sql_query(text(sql_query), connection)
+        if df.empty:
+            # Log a message or handle the empty DataFrame scenario as needed
+            msg = "The query returned an empty DataFrame."
+            logger.error(msg)
+            raise ValueError(msg)
+        logger.info("Query executed successfully.")
+        return df
+    except ValueError as e:
+        logger.error(f"SQL query failed. Error: {e}")
+        raise e
+    except Exception as e:
+        logger.error(f"An error occurred while querying the database. Error: {e}")
+        raise e
 
-weather_data_URL = "https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Maji_Ndogo/Weather_station_data.csv"
-weather_mapping_data_URL = "https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Maji_Ndogo/Weather_data_field_mapping.csv"
-
-def load_data_from_db(db_path, sql_query):
-    """
-    Loads data from a SQLite database using the provided SQL query.
-
-    Args:
-        db_path (str): Path to the SQLite database file.
-        sql_query (str): SQL query to execute.
-
-    Returns:
-        pandas.DataFrame: DataFrame containing the results of the query.
-    """
-
-    engine = sa.create_engine(db_path)
-    df = pd.read_sql_query(sql_query, engine)
-    return df
+def read_from_web_CSV(URL):
+    try:
+        df = pd.read_csv(URL)
+        logger.info("CSV file read successfully from the web.")
+        return df
+    except pd.errors.EmptyDataError as e:
+        logger.error("The URL does not point to a valid CSV file. Please check the URL and try again.")
+        raise e
+    except Exception as e:
+        logger.error(f"Failed to read CSV from the web. Error: {e}")
+        raise e
